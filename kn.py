7b6e63130d451dcaa53a997a05a7afb71a4a3469
Soja-hub <62591947+Soja-hub@users.noreply.github.com>
2020-05-10 15:29:07 +0200

    Update kn.py

--- kn.py
+++ kn.py
@@ -268,4 +268,32 @@ async def ping(ctx):
     ms = (t.timestamp - ctx.message.timestamp).total_seconds() * 1000
     await t.edit(new_content=f'Pong! Latence : **{int(ms)} milliseconds** !')
 
+@bot.command()
+async def mute(ctx,member : discord.Member):
+  guild = ctx.guild
+  for role in guild.roles:
+    if role.name —- "Muted":
+      await member.add.roles(role)
+      await ctx.send("{} has {} has been muted".format(member.mention,ctx.author.mention))
+      return 
+
+      overwrite = discord.PermissionsOverwrite(send_messages=False)
+      newRole = await guild.create_role(name="Muted")
+
+
+      for channel in guild.text_channels: 
+      await channel.set_permissions(newRole,overwrite=overwrite)
+    await member.add_roles(newRoles)
+    await ctx.send(" {} has {} has been muted ".format.member.mention,ctx.author.mention))
+
+@bot.command()
+async def unmute(ctx,member.discord.Member):
+  guild = ctx.guild
+  
+  for role in guild.roles:
+   if role.name —- "Muted"
+    await member.remove_roles(role)
+    await ctx.send(" {} has {} has been unmuted ".format(member.mention,ctx.author.mention))
+    return
+
 bot.run(os.environ['TOKEN'])
